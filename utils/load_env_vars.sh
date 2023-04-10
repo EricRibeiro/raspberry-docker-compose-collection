aa230516bef5853f4d2c19ee6a75ed2e71f3c9cc
Eric Ribeiro <ericribeiro@outlook.com.br>
2023-04-09 23:21:54 -0700

    feat: add `qBittorrent` to the collection

--- utils/load_env_vars.sh
+++ utils/load_env_vars.sh
@@ -9,14 +9,23 @@
 # Get the absolute directory of the script itself
 script_dir="$(cd "$(dirname "$0")" && pwd)"
 
+# Function to export variables from a file
+export_variables_from_file() {
+  local -r file="$1"
+  # Source the file
+  source "$file"
+  # Export the variables
+  while IFS="=" read -r key _; do export "${key}"; done < "$file"
+}
+
 # Load global variables
 readonly global_env="${script_dir}/../global.env"
 [ -f "$global_env" ] || { printf "%s\n" "Please ensure \"global.env.example\" was renamed to \"global.env\" and populated."; exit 1; }
 printf "%s\n" "Loading global environment variables from \"${global_env}\"..."
-source "$global_env"
+export_variables_from_file "$global_env"
 
 # Load local variables
 readonly local_env="${script_dir}/.env"
 [ -f "$local_env" ] || { printf "%s\n" "Please ensure \"env.example\" was renamed to \".env\" and populated."; exit 1; }
 printf "%s\n" "Loading local environment variables from \"${local_env}\"..."
-source "$local_env"
+export_variables_from_file "$local_env"
